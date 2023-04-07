6b5c370886ff290b632951921e8466a2e3d47f98
Jeison Muniz <jsnprogramer@gmail.com>
2023-04-07 00:02:40 -0300

    implementation of proxy pattern

---
+++ structural/proxy.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+from abc import ABC  , abstractmethod
+
+
+class Subject(ABC):
+    
+    @abstractmethod
+    def request(self) -> None :
+        pass
+
+    
+    
+
+class RealSubject(Subject):
+    
+    
+    def request(self) -> None:
+        print(" REALSUBJECT : HANDLER REQUEST")
+       
+
+
+
+class Proxy(Subject):
+    
+    
+    def __ini__(self , realSubject: RealSubject) -> None:
+        self._realSubject = realSubject 
+        
+     
+        
+    def request(self) -> None:
+        print(realSubject.request())
+        
+        
+
+
+if __name__ == "__main__":
+    
+    realSubject = RealSubject()
+    
+    proxy = Proxy(realSubject)
+    
+    print(proxy.request())
